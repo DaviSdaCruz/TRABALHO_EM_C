94835c601fd8aeb9ae0842e3b8d5757f47fc39de
DaviSdaCruz <daviszeremeta@gmail.com>
2025-11-19 09:50:51 -0300

    Add files via upload

--- pong/main.c
+++ pong/main.c
@@ -1,4 +1,4 @@
-// --- TRABALHO DE PROGRAMA��O DE COMPUTADORES I ---
+// --- TRABALHO DE PROGRAMAÇÃO DE COMPUTADORES I ---
 // --- ALUNO: DAVI SZEREMETA DA CRUZ             ---
 // --- RA: 25106623                              ---
 // --- CURSO: ENGENHARIA DE SOFTWARE(NOTURNO)    ---
@@ -16,225 +16,246 @@ int main() {
 
     while (!WindowShouldClose()) {
 
+        // -------------------- MENU --------------------
         if (interface == MENU) {
+
             if (IsKeyPressed(KEY_DOWN)) selecionado++;
-            if (IsKeyPressed(KEY_UP)) selecionado--;
+            if (IsKeyPressed(KEY_UP))   selecionado--;
             if (selecionado > 2) selecionado = 0;
             if (selecionado < 0) selecionado = 2;
 
             if (IsKeyPressed(KEY_ENTER)) {
-                switch (selecionado) {
-                    case 0: interface = DOIS_JOGADORES; break;
-                    case 1: interface = UM_JOGADOR; break;
-                    case 2: interface = SAIR; break;
-                }
+                if (selecionado == 0) interface = DOIS_JOGADORES;
+                if (selecionado == 1) interface = UM_JOGADOR;
+                if (selecionado == 2) interface = SAIR;
             }
+
+            BeginDrawing();
+            ClearBackground(DARKGREEN);
+
+            DrawText("P O N G", 80, 160, 160, WHITE);
+            DrawRectangleLines(45, 145, 700, 165, WHITE);
+            DrawText("Use as setas e ENTER para escolher", 50, 330, 20, BLACK);
+
+            DrawText("CONTROLES:", 20, 810, 30, WHITE);
+            DrawText("Jogador esquerdo: teclas W e S", 20, 840, 28, BLACK);
+            DrawText("Jogador direito: setas do teclado", 20, 870, 28, BLACK);
+
+            Color c1 = (selecionado == 0) ? YELLOW : WHITE;
+            Color c2 = (selecionado == 1) ? YELLOW : WHITE;
+            Color c3 = (selecionado == 2) ? YELLOW : WHITE;
+
+            DrawText("Dois Jogadores", 1240, 390, 50, c1);
+            DrawText("Um Jogador", 1240, 470, 50, c2);
+            DrawText("Sair", 1240, 550, 50, c3);
+            DrawRectangleLines(1200, 297, 470, 390, WHITE);
+
+            EndDrawing();
         }
 
         if (interface == SAIR) break;
 
 
-        BeginDrawing();
-        ClearBackground(BLACK);
 
-        if (interface == MENU) {
-
-    DrawText("P O N G", 80, 160, 160, RAYWHITE);
-    DrawRectangleLines(45, 145, 700, 165, RAYWHITE);
-    DrawText("Use as setas e ENTER para escolher", 50, 330, 20, GRAY);
-
-
-    DrawText("CONTROLES:", 20, 780, 30, WHITE);
-    DrawText("Jogador esquerdo: teclas W e S", 20, 810, 28, RED);
-    DrawText("Jogador direito: setas do teclado", 20, 840, 28, BLUE);
-    DrawText("Pressione ESC para voltar ao menu", 20, 870, 28, PURPLE);
-
-
-    Color c1 = (selecionado == 0) ? YELLOW : RAYWHITE;
-    Color c2 = (selecionado == 1) ? YELLOW : RAYWHITE;
-    Color c3 = (selecionado == 2) ? YELLOW : RAYWHITE;
-
-    DrawText("Dois Jogadores", 1240, 390, 50, c1);
-    DrawText("Um Jogador", 1240, 470, 50, c2);
-    DrawText("Sair", 1240, 550, 50, c3);
-    DrawRectangleLines(1200, 297, 470, 390, RAYWHITE);
-}
-
-
+        // --------------------- DOIS JOGADORES ---------------------
         if (interface == DOIS_JOGADORES) {
 
-    SetTargetFPS(60);
+            Texture2D fundo = LoadTexture("fundo.png");
 
+            Vector2 bola = {900, 425};
+            Vector2 velocidadebola = {10, 10};
+            Rectangle jogador1 = {50, 400, 20, 200};
+            Rectangle jogador2 = {1730, 400, 20, 200};
+            int pontos1 = 0, pontos2 = 0;
 
-    Vector2 bola = {900, 425};
-    Vector2 velocidadebola = {10, 10};
-    Rectangle jogador1 = {50, 400, 20, 200};
-    Rectangle jogador2 = {1730, 400, 20, 200};
-    int pontos1 = 0, pontos2 = 0;
+            while (!WindowShouldClose()) {
 
-    while (!WindowShouldClose()) {
+                // ----------- MOVIMENTOS -----------
+                if (IsKeyDown(KEY_W) && jogador1.y > 0) jogador1.y -= 12;
+                if (IsKeyDown(KEY_S) && jogador1.y < GetScreenHeight() - jogador1.height) jogador1.y += 12;
+                if (IsKeyDown(KEY_UP) && jogador2.y > 0) jogador2.y -= 12;
+                if (IsKeyDown(KEY_DOWN) && jogador2.y < GetScreenHeight() - jogador2.height) jogador2.y += 12;
 
-        if (IsKeyDown(KEY_W) && jogador1.y > 0) jogador1.y -= 12;
-        if (IsKeyDown(KEY_S) && jogador1.y < GetScreenHeight() - jogador1.height) jogador1.y += 12;
-        if (IsKeyDown(KEY_UP) && jogador2.y > 0) jogador2.y -= 12;
-        if (IsKeyDown(KEY_DOWN) && jogador2.y < GetScreenHeight() - jogador2.height) jogador2.y += 12;
+                // ----------- FÍSICA DA BOLA -----------
+                bola.x += velocidadebola.x;
+                bola.y += velocidadebola.y;
 
+                float aumentarvelocidade = 1.02f;
 
-        bola.x += velocidadebola.x;
-        bola.y += velocidadebola.y;
+                if (bola.y <= 0 || bola.y >= GetScreenHeight()) {
+                    velocidadebola.y *= -1;
+                    velocidadebola.x *= aumentarvelocidade;
+                    velocidadebola.y *= aumentarvelocidade;
+                }
 
-        float aumentarvelocidade = 1.02f;
+                if (CheckCollisionCircleRec(bola, 16, jogador1) && velocidadebola.x < 0) {
+                    velocidadebola.x *= -1;
+                    velocidadebola.x *= aumentarvelocidade;
+                    velocidadebola.y *= aumentarvelocidade;
+                }
 
+                if (CheckCollisionCircleRec(bola, 16, jogador2) && velocidadebola.x > 0) {
+                    velocidadebola.x *= -1;
+                    velocidadebola.x *= aumentarvelocidade;
+                    velocidadebola.y *= aumentarvelocidade;
+                }
 
-if (bola.y <= 0 || bola.y >= GetScreenHeight()) {
-    velocidadebola.y *= -1;
-    velocidadebola.x *= aumentarvelocidade;
-    velocidadebola.y *= aumentarvelocidade;
-}
+                if (bola.x <= 0) {
+                    pontos2++;
+                    bola = (Vector2){900, 425};
 
+                    float dirX = (GetRandomValue(0, 1) == 0) ? -1 : 1;
+                    float dirY = (GetRandomValue(0, 1) == 0) ? -1 : 1;
+                    velocidadebola = (Vector2){10 * dirX, 10 * dirY};
+                }
 
-if (CheckCollisionCircleRec(bola, 16, jogador1) && velocidadebola.x < 0) {
-    velocidadebola.x *= -1;
-    velocidadebola.x *= aumentarvelocidade;
-    velocidadebola.y *= aumentarvelocidade;
-}
+                if (bola.x >= GetScreenWidth()) {
+                    pontos1++;
+                    bola = (Vector2){900, 425};
 
-if (CheckCollisionCircleRec(bola, 16, jogador2) && velocidadebola.x > 0) {
-    velocidadebola.x *= -1;
-    velocidadebola.x *= aumentarvelocidade;
-    velocidadebola.y *= aumentarvelocidade;
-}
+                    float dirX = (GetRandomValue(0, 1) == 0) ? -1 : 1;
+                    float dirY = (GetRandomValue(0, 1) == 0) ? -1 : 1;
+                    velocidadebola = (Vector2){10 * dirX, 10 * dirY};
+                }
 
+                // ----------- DESENHO -----------
+                BeginDrawing();
+                ClearBackground(BLACK);
 
-        if (bola.x <= 0) {
-        pontos2++;
-        bola = (Vector2){900, 425};
+                DrawTexturePro(
+                    fundo,
+                    (Rectangle){0, 0, fundo.width, fundo.height},
+                    (Rectangle){0, 0, GetScreenWidth(), GetScreenHeight()},
+                    (Vector2){0, 0},
+                    0,
+                    WHITE
+                );
 
-            float dirX = (GetRandomValue(0, 1) == 0) ? -1.0f : 1.0f;
-            float dirY = (GetRandomValue(0, 1) == 0) ? -1.0f : 1.0f;
+                DrawCircleV(bola, 16, ORANGE);
+                DrawRectangleRec(jogador1, RED);
+                DrawRectangleRec(jogador2, BLUE);
+                DrawText(TextFormat("%d", pontos1), GetScreenWidth()/4, 40, 60, WHITE);
+                DrawText(TextFormat("%d", pontos2), 3*GetScreenWidth()/4, 40, 60, WHITE);
 
-        velocidadebola = (Vector2){10 * dirX, 10 * dirY};
-}
+                EndDrawing();
+            }
 
-        if (bola.x >= GetScreenWidth()) {
-        pontos1++;
-        bola = (Vector2){900, 425};
-
-
-            float dirX = (GetRandomValue(0, 1) == 0) ? -1.0f : 1.0f;
-            float dirY = (GetRandomValue(0, 1) == 0) ? -1.0f : 1.0f;
-
-        velocidadebola = (Vector2){10 * dirX, 10 * dirY};
-}
-
-
-        BeginDrawing();
-            ClearBackground(BLACK);
-            DrawLine(GetScreenWidth()/2, 0, GetScreenWidth()/2, GetScreenHeight(), GRAY);
-            DrawCircleV(bola, 16, RAYWHITE);
-            DrawRectangleRec(jogador1, RED);
-            DrawRectangleRec(jogador2, BLUE);
-            DrawText(TextFormat("%d", pontos1), GetScreenWidth()/4, 40, 60, WHITE);
-            DrawText(TextFormat("%d", pontos2), 3*GetScreenWidth()/4, 40, 60, WHITE);
-
-        EndDrawing();
-    }
-
-            if (IsKeyPressed(KEY_ESCAPE)) interface = MENU;
         }
+
+
+
+        // --------------------- UM JOGADOR ---------------------
         if (interface == UM_JOGADOR) {
 
-    SetTargetFPS(60);
+            Texture2D fundo = LoadTexture("fundo.png");
 
-    Vector2 bola = {900, 425};
-    Vector2 velocidadebola = {10, 10};
-    Rectangle jogador1 = {50, 400, 20, 200};
-    Rectangle ia = {1730, 400, 20, 200};
-    int pontos1 = 0, pontos2 = 0;
+            Vector2 bola = {900, 425};
+            Vector2 velocidadebola = {10, 10};
+            Rectangle jogador1 = {50, 400, 20, 200};
+            Rectangle ia = {1730, 400, 20, 200};
 
-    while (!WindowShouldClose()) {
+            int pontos1 = 0, pontos2 = 0;
 
-        if (IsKeyDown(KEY_W) && jogador1.y > 0) jogador1.y -= 12;
-        if (IsKeyDown(KEY_S) && jogador1.y < GetScreenHeight() - jogador1.height) jogador1.y += 12;
+            while (!WindowShouldClose()) {
 
-        if (bola.y < ia.y + ia.height / 2) ia.y -= 16;
-        if (bola.y > ia.y + ia.height / 2) ia.y += 16;
+                // MOVIMENTO DO JOGADOR
+                if (IsKeyDown(KEY_W) && jogador1.y > 0) jogador1.y -= 12;
+                if (IsKeyDown(KEY_S) && jogador1.y < GetScreenHeight() - jogador1.height) jogador1.y += 12;
 
-        if (ia.y < 0) ia.y = 0;
-        if (ia.y > GetScreenHeight() - ia.height) ia.y = GetScreenHeight() - ia.height;
+                // IA DO OPONENTE
+                if (bola.y < ia.y + ia.height/2) ia.y -= 16;
+                if (bola.y > ia.y + ia.height/2) ia.y += 16;
 
+                if (ia.y < 0) ia.y = 0;
+                if (ia.y > GetScreenHeight() - ia.height)
+                    ia.y = GetScreenHeight() - ia.height;
 
-        bola.x += velocidadebola.x;
-        bola.y += velocidadebola.y;
+                // FÍSICA DA BOLA
+                bola.x += velocidadebola.x;
+                bola.y += velocidadebola.y;
 
-        float aumentarvelocidade = 1.02f;
+                float aumentarvelocidade = 1.02f;
 
+                if (bola.y <= 0 || bola.y >= GetScreenHeight()) {
+                    velocidadebola.y *= -1;
+                    velocidadebola.x *= aumentarvelocidade;
+                    velocidadebola.y *= aumentarvelocidade;
+                }
 
-if (bola.y <= 0 || bola.y >= GetScreenHeight()) {
-    velocidadebola.y *= -1;
-    velocidadebola.x *= aumentarvelocidade;
-    velocidadebola.y *= aumentarvelocidade;
+                if (CheckCollisionCircleRec(bola, 16, jogador1) && velocidadebola.x < 0) {
+                    velocidadebola.x *= -1;
+                    velocidadebola.x *= aumentarvelocidade;
+                    velocidadebola.y *= aumentarvelocidade;
+                }
 
-}
+                if (CheckCollisionCircleRec(bola, 16, ia) && velocidadebola.x > 0) {
+                    velocidadebola.x *= -1;
+                    velocidadebola.x *= aumentarvelocidade;
+                    velocidadebola.y *= aumentarvelocidade;
+                }
 
+                // PONTUAÇÃO
+                if (bola.x <= 0) {
+                    pontos2++;
+                    bola = (Vector2){900, 425};
 
-if (CheckCollisionCircleRec(bola, 16, jogador1) && velocidadebola.x < 0) {
-    velocidadebola.x *= -1;
-    velocidadebola.x *= aumentarvelocidade;
-    velocidadebola.y *= aumentarvelocidade;
+                    float dirX = (GetRandomValue(0, 1) == 0) ? -1 : 1;
+                    float dirY = (GetRandomValue(0, 1) == 0) ? -1 : 1;
 
-}
+                    velocidadebola = (Vector2){10 * dirX, 10 * dirY};
+                }
 
-if (CheckCollisionCircleRec(bola, 16, ia) && velocidadebola.x > 0) {
-    velocidadebola.x *= -1;
-    velocidadebola.x *= aumentarvelocidade;
-    velocidadebola.y *= aumentarvelocidade;
+                if (bola.x >= GetScreenWidth()) {
+                    pontos1++;
+                    bola = (Vector2){900, 425};
 
-}
+                    float dirX = (GetRandomValue(0, 1) == 0) ? -1 : 1;
+                    float dirY = (GetRandomValue(0, 1) == 0) ? -1 : 1;
 
-        if (bola.x <= 0) {
-        pontos2++;
-        bola = (Vector2){900, 425};
+                    velocidadebola = (Vector2){10 * dirX, 10 * dirY};
+                }
 
-            float dirX = (GetRandomValue(0, 1) == 0) ? -1.0f : 1.0f;
-            float dirY = (GetRandomValue(0, 1) == 0) ? -1.0f : 1.0f;
+                // DESENHO
+                BeginDrawing();
+                ClearBackground(BLACK);
 
-        velocidadebola = (Vector2){10 * dirX, 10 * dirY};
-}
+                DrawTexturePro(
+                    fundo,
+                    (Rectangle){0, 0, fundo.width, fundo.height},
+                    (Rectangle){0, 0, GetScreenWidth(), GetScreenHeight()},
+                    (Vector2){0, 0},
+                    0,
+                    WHITE
+                );
 
-        if (bola.x >= GetScreenWidth()) {
-        pontos1++;
-        bola = (Vector2){900, 425};
+                DrawCircleV(bola, 16, ORANGE);
+                DrawRectangleRec(jogador1, RED);
+                DrawRectangleRec(ia, BLUE);
 
+                DrawText(TextFormat("%d", pontos1), GetScreenWidth()/4, 40, 60, WHITE);
+                DrawText(TextFormat("%d", pontos2), 3*GetScreenWidth()/4, 40, 60, WHITE);
 
-            float dirX = (GetRandomValue(0, 1) == 0) ? -1.0f : 1.0f;
-            float dirY = (GetRandomValue(0, 1) == 0) ? -1.0f : 1.0f;
+                EndDrawing();
+            }
 
-        velocidadebola = (Vector2){10 * dirX, 10 * dirY};
-}
-
-        BeginDrawing();
-            ClearBackground(BLACK);
-            DrawLine(GetScreenWidth()/2, 0, GetScreenWidth()/2, GetScreenHeight(), GRAY);
-            DrawCircleV(bola, 16, RAYWHITE);
-            DrawRectangleRec(jogador1, RED);
-            DrawRectangleRec(ia, BLUE);
-            DrawText(TextFormat("%d", pontos1), GetScreenWidth()/4, 40, 60, WHITE);
-            DrawText(TextFormat("%d", pontos2), 3*GetScreenWidth()/4, 40, 60, WHITE);
-
-        EndDrawing();
-    }
-
-
-            if (IsKeyPressed(KEY_ESCAPE)) interface = MENU;
         }
 
-        EndDrawing();
     }
 
     CloseWindow();
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
 
 
 
